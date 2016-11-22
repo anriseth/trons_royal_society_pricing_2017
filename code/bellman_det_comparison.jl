935d9240420d6f1467ed94ae39a436d4ce024030
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2016-11-22 16:13:26 +0000

    Update heatmap plotting code

--- code/bellman_det_comparison.jl
+++ code/bellman_det_comparison.jl
@@ -45,8 +45,6 @@ function creatediffarrays()
     for C in Carr, γ in γarr
         counter += 1
         @show counter
-        # diffarrays = Vector{Array{Float64,2}}(length(q1arr)*length(q2arr))
-        # for q1 in q1arr, q2 in q2arr
 
         diffarray = zeros(length(q2arr),length(q1arr))
         for i = 1:length(q1arr)
@@ -54,24 +52,20 @@ function creatediffarrays()
             for j = 1:length(q2arr)
                 q2 = q2arr[j]
 
-                #diffarray = zeros(length(γarr),length(Carr))
-                # for i = 1:length(Carr)
-                #     C = Carr[i]
-                #     for j = 1:length(γarr)
-                #         γ = γarr[j]
-
                 @simd for k = 1:length(xarr)
                     objective(a) = -F(a,xarr[k],C,γ,q1,q2)
                     res = optimize(objective, amin, amax)
                     α[k] = Optim.minimizer(res)
-                    # objective2(a) = -monteF(a,xarr[k],C,γ,q1,q2)
-                    # res2 = optimize(objective2, amin, amax)
-                    # α2[k] = Optim.minimizer(res2)
+                    #v[k] = -Optim.minimum(res)
                 end
 
                 αcec(s) = min(amax,max(log(q1./s)/q2,1/q2-C,amin))
+                #vcec(s) = (αcec(s)+C).*min(q(αcec(s),q1,q2),s) - C*s
                 αcecarr[:] = αcec(xarr)
-
+                # for k = 1:length(vcecarr)
+                #     vcecarr[k] = vcec(xarr[k])
+                # end
+                #diffarray[j,i] = maximum(v-vcecarr)
                 diffarray[j,i] = norm(αcecarr-α, 2)/sqrt(length(α)) # Normalised 2-norm
                 #diffarrayinf[j,i] = norm(αcecarr-α, Inf) # Inf-norm
             end
@@ -85,15 +79,37 @@ end
 diffarrays = creatediffarrays()
 
 
+function diffv(v,vcecarr,α,αcecarr,C,γ,q1,q2)
+    @simd for k = 1:length(xarr)
+        objective(a) = -F(a,xarr[k],C,γ,q1,q2)
+        res = optimize(objective, amin, amax)
+        v[k] = -Optim.minimum(res)
+        α[k] = Optim.minimizer(res)
+    end
+
+
+    αcec(s) = min(amax,max(log(q1./s)/q2,1/q2-C,amin))
+    vcec(s) = (αcec(s)+C).*min(q(αcec(s),q1,q2),s) - C*s
+    for k = 1:length(vcecarr)
+        vcecarr[k] = vcec(xarr[k])
+        αcecarr[k] = αcec(xarr[k])
+    end
+end
+
+
+
 using JLD
 
-savefiles = true
+savefiles = false
 if savefiles == true
     @save "./data/bellman_det_comparison_$(now()).jld"
 end
 
-Plots.scalefontsizes(0.8)
+Plots.scalefontsizes(1.5)
 titles = [L"$(C,\gamma)=(0.5,0.05)$" L"$(C,\gamma)=(0.5,0.1)$" L"$(C,\gamma)=(1,0.05)$" L"$(C,\gamma)=(1,0.1)$"]
 plt = plot(q1arr, q2arr, diffarrays, st=:heatmap, margin=0mm, ratio=0.4,
            xlabel=L"$q_1$", ylabel=L"$q_2$",
            title=titles,layout=@layout [ p11 p12 ; p21 p22])
+scatter!(plt.spmap[:p21], [q1],[q2], label="", color=:white, markersize=10)
+xlims!(plt.spmap[:p21], minimum(q1arr), maximum(q1arr))
+ylims!(plt.spmap[:p21], minimum(q2arr), maximum(q2arr))
