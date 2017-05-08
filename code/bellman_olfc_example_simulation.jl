943984ede5c96952e27bf9c709ce9afe9ed70403
Asbjørn Nilsen Riseth <riseth@maths.ox.ac.uk>
2017-05-08 23:27:58 +0100

    OLFC via solveolfc!

--- code/bellman_olfc_example_simulation.jl
+++ code/bellman_olfc_example_simulation.jl
@@ -38,7 +38,7 @@ T = 3
 amin = 0.; amax = 1.
 system = DynamicSystem1D(f,U,Ubar,T,amin,amax)
 xmin = 0.; xmax = 1.
-K = 301
+K = 201
 xarr = collect(linspace(xmin,xmax,K))
 xtup = (xarr,)
 
@@ -54,116 +54,34 @@ v = zeros(K, T+1)
 α = zeros(K, T)
 
 @time solvebellman!(v, α, system, xtup, ω)
+αolfc = copy(α)
 
-function simulatevalues(olfc::OLFCSystem1D,
-                        osc::OfflineSystemControl1D,
-                        ωmodel::UnivariateDistribution,
-                        x0,
-                        numsimulations::Int = 1000,
-                        includemean::Bool = true;
-                        ωtrue::UnivariateDistribution = ωmodel,
-                        verbose::Bool = false,
-                        optimizer::Optim.Optimizer = GradientDescent(linesearch=LineSearches.bt3!),
-                        aguessinit::Vector = fill(0.5*(olfc.system.amin+olfc.system.amax), olfc.system.T))
-    @assert olfc.system === osc.system
-    system = olfc.system
-
-    oscvals = zeros(numsimulations)
-    olfcvals = zeros(numsimulations)
-
-    for sim = 1:numsimulations
-        trajosc = DynamicSystemTrajectory1D(system)
-        trajolfc = DynamicSystemTrajectory1D(system)
-        initializestate!(trajosc, x0)
-        initializestate!(trajolfc, x0)
-        aguess = copy(aguessinit)
-        atolfc = aguess
-
-        for t = 0:system.T-1
-            numscenarios = olfc.numscenarios[t+1]
-            includemean = includemean || (numscenarios == 1) # Always predict the future using the mean when only one sample
-            if t > 0
-                aguess = atolfc[2:end]
-            end
-
-            atosc = osc.policy(t, trajosc.state[t+1])
-            atolfc = onlinedecision(olfc, trajolfc.state[t+1], t, aguess, ωmodel, includemean;
-                                    verbose = verbose, optimizer = optimizer)
-
-            # Update guess for next time
-            w = rand(ωtrue)
-            step!(trajosc, atosc, t, w)
-            step!(trajolfc, atolfc[1], t, w)
-        end
-        oscvals[sim] = trajosc.value[end]
-        olfcvals[sim] = trajolfc.value[end]
-    end
-
-    return oscvals, olfcvals
-end
-
-function getvalsarrs(olfcnumscenariosarr, numsimulations, bellman, x0)
-    bellmanvalsarr = Vector{Vector}()
-    olfcvalsarr = Vector{Vector}()
-    for olfcnumscenarios in olfcnumscenariosarr
-        @show olfcnumscenarios
-        olfc = OLFCSystem1D(system, x0, olfcnumscenarios)
-
-        @time (bellmanvals,
-               olfcvals) = simulatevalues(olfc, bellman,
-                                          ωdist, x0,
-                                          numsimulations,
-                                          olfcnumscenarios == 1;
-                                          verbose = false,
-                                          optimizer = GradientDescent(linesearch=LineSearches.bt3!),#LBFGS(linesearch=LineSearches.bt3!),
-                                          aguessinit = fill(α[end,1],system.T))
-        push!(bellmanvalsarr, bellmanvals)
-        push!(olfcvalsarr, olfcvals)
-    end
-    return bellmanvalsarr, olfcvalsarr
-end
 
 # Simulations
-
 x0 = xarr[end]
 bellman = OfflineSystemControl1D(system, x0, xarr, α)
-olfcnumscenariosarr = [100]
-numsimulations = 1000
 
-(bellmanvals,
- olfcvals) = getvalsarrs(olfcnumscenariosarr,
-                         numsimulations, bellman, x0)
+olfc = OLFCSystem1D(system, x0, -1)
+#olfcw = fill(mean(ωdist),T,1)
+@time solveolfc!(αolfc,olfc,xtup,ω';
+                 optimizer=GradientDescent, linesearch = LineSearches.bt3!)
+
+olfccon = OfflineSystemControl1D(system, x0, xarr, αolfc)
+
+numsimulations = 10000
+
+srand(1) # Reproducibility indep of bellmansamples etc
+(bellmantrajectories,
+ olfctrajectories) = simulatetrajectories([bellman, olfccon],
+                                          ωdist, x0,
+                                          numsimulations)
+
+bellmanvals = [traj.value[end] for traj in bellmantrajectories]
+olfcvals = [traj.value[end] for traj in olfctrajectories]
 
 using JLD
-savefiles = false
+savefiles = true
 if savefiles == true
     @save "./data/markdown_bellman_olfc_$(now()).jld" bellmanvals olfcvals
-    writecsv("./data/markdown_bellman_olfc_5_20_100.csv", [bellmanvals... olfcvals...])
+    writecsv("./data/markdown_bellman_olfc_1000.csv", [bellmanvals... olfcvals...])
 end
-
-#density(bellmanvals-olfcvals)
-
-#==
-@load("./data/bellman_rollout.jld")
-diffarr = bellmanplotvals-olfcplotvals
-writecsv("./data/markdown_bellman_olfc_20_100_500.csv", diffarr)
-==#
-
-#==
-# Remember to load the file in the following way:
-using DiscreteControl
-using Optim
-using Distributions
-@load "./data/markdown_bellman_olfc_<date>.jld"
-==#
-
-#cdensity((bellmanvals-olfcvals)[1]); for i = 2:4; cdensity!((bellmanvals-olfcvals)[i]); end
-#gui()
-
-
-# using StatPlots
-# density(bellmanvals-olfcvals,
-#         title = "Bellman vs. OLFC"
-#         xlabel="Value of Bellman vs. OLFC",
-#         ylabel = "Density",
-#         label="")
---
+++ code/bellman_olfc_tablestats.jl
@@ -0,0 +1,160 @@
+using DiscreteControl
+using Optim
+using Distributions
+import Distributions: rand, mean
+using LaTeXStrings
+
+if !isdefined(:Betashift)
+    type Betashift{T} <: Distributions.Distribution{Univariate,Continuous}
+        rv::Distributions.Beta{T}
+        shift::T
+    end
+
+    function rand(d::Betashift)
+        d.shift + rand(d.rv)
+    end
+    function rand(d::Betashift, dims::Int...)
+        d.shift + rand(d.rv, dims...)
+    end
+
+    function mean(d::Betashift)
+        d.shift + mean(d.rv)
+    end
+end
+
+
+if !isdefined(:DemandFun)
+    type DemandFun{T}
+        q1::T
+        q2::T
+    end
+end
+
+(d::DemandFun{T}){T}(a) = d.q1*exp(-d.q2*a)
+q = DemandFun(1.0,1.0)
+
+function main(;bellsamples = 1000, olfcsamples = -1)
+    # Paramarr
+    Carr = [0.25, 0.25, 0.5, 0.5, 1.0, 1.0]
+    γarr = [5e-2, 1e-1,5e-2, 1e-1,5e-2, 1e-1]
+    q1s = [1+1/3, 2, 2+2/3]
+    q2s = [2+2/3, 4]
+    q1arr = [q1s[k] for k in [2,1,3,2,1,3]]
+    q2arr = [q2s[k] for k in [2,1,2,1,2,1]]
+
+    amin = 0.; amax = 1.
+    xmin = 0.; xmax = 1.
+    K = 201
+    xarr = collect(linspace(xmin,xmax,K))
+    xtup = (xarr,)
+    T = 3
+
+    numsimulations = 10000
+    x0 = 1.0
+
+    srand(0) # For reproducibility
+
+    function createsamples(bellvalarrs, olfcvalarrs)
+        counter = 0
+
+        for k = 1:length(Carr)
+            C = Carr[k]; γ = γarr[k]; q1 = q1arr[k]; q2 = q2arr[k]
+            q.q1 = q1; q.q2 = q2
+
+            counter += 1
+            @show counter
+
+            betavar = 1./(8γ^2)-0.5  # Sets variance equal to γ^2
+            Ubar(x) = -C*x
+
+            f(t,x,a,w) = x-min(q(a)*w,x)
+            U(t,x,a,w) = a*min(q(a)*w,x)
+
+            system = DynamicSystem1D(f,U,Ubar,T,amin,amax)
+
+            ωdist = Betashift(Beta(betavar,betavar), 0.5) # shift by 0.5 to get mean 1
+
+            srand(0) # For reproducibility
+            ω = rand(ωdist,bellsamples,T)
+            # Value and policy function arrays
+            v = zeros(K, T+1)
+            α = zeros(K, T)
+            αolfc = copy(α)
+
+            @time solvebellman!(v, α, system, xtup, ω)
+
+            # Simulations
+            bellman = OfflineSystemControl1D(system, x0, xarr, α)
+
+            olfc = OLFCSystem1D(system, x0, olfcsamples)
+            if olfcsamples == -1
+                @time solveolfc!(αolfc,olfc,xtup,ω';
+                                 optimizer=GradientDescent, linesearch = LineSearches.bt3!)
+            else
+                wolfc = rand(ωdist, T, olfcsamples)
+                @time solveolfc!(αolfc,olfc,xtup,wolfc;
+                                 optimizer=GradientDescent, linesearch = LineSearches.bt3!)
+            end
+
+            olfccon = OfflineSystemControl1D(system, x0, xarr, αolfc)
+
+            srand(1) # start with same underlying samples each time
+
+            (bellmantrajectories,
+             olfctrajectories) = simulatetrajectories([bellman, olfccon],
+                                                      ωdist, x0,
+                                                      numsimulations)
+
+            bellmanvals = [traj.value[end] for traj in bellmantrajectories]
+            olfcvals = [traj.value[end] for traj in olfctrajectories]
+
+
+            bellvalarrs[:,k] = bellmanvals
+            olfcvalarrs[:,k] = olfcvals
+
+        end
+    end
+
+    function printtable(bellvalarrs, olfcvalarrs)
+        numentries = size(bellvalarrs,2)
+        diffarr = 1.0 - olfcvalarrs./bellvalarrs
+        diffnorms = [norm((olfcvalarrs-bellvalarrs)[:,k],2)/norm(bellvalarrs[:,k],2)
+                     for k = 1:numentries]
+        vals = [quantile(diffarr[:,k], q)
+                for q = [0.05,0.5,0.95], k = 1:numentries]
+        vals = round(1000*[vals; diffnorms'], 1) # Show % to nearest 1/10000
+
+        str = """
+\\begin{tabular}{llllcccc}
+  \$C\$ & \$\\gamma\$ & \$q_1\$ & \$q_2\$ & \$\\mathcal Q_{0.05}\$
+  &Median & \$\\mathcal Q_{0.95}\$ &\$L_2\$\\\\
+  \\toprule
+"""
+        for k = 1:numentries
+            C = round(Carr[k],2); γ = round(γarr[k],2); q1 = round(q1arr[k],2); q2 = round(q2arr[k],2)
+            valstrs = prod(["& \$$v\$ " for v = vals[:,k]])
+            str *= "\$$C\$ & \$$γ\$ & \$$q1\$ & \$$q2\$ " *
+                valstrs * "\\\\\n"
+        end
+        str *= """
+  &&&&\$\\times 10^{-3}\$&\$\\times 10^{-3}\$&\$\\times 10^{-3}\$&\$\\times 10^{-3}\$\\\\
+  \\bottomrule
+\\end{tabular}
+"""
+    end
+
+    bellvalarrs = Array{Float64,2}(numsimulations, length(Carr))
+    olfcvalarrs = Array{Float64,2}(numsimulations, length(Carr))
+    createsamples(bellvalarrs, olfcvalarrs)
+    tblstr = printtable(bellvalarrs, olfcvalarrs)
+
+    return bellvalarrs, olfcvalarrs, tblstr
+end
+
+bellvalarrs, olfcvalarrs, tblstr = main()
+
+print(tblstr)
+
+function plothistos(bellvalarrs, olfcvalarrs)
+    Base.error("Not finished")
+end
